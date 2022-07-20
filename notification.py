734263776009c4bf4ad8e6caeb13ae2dd40d8e19
8a8al00ey <109389709+8a8al00ey@users.noreply.github.com>
2022-07-20 13:32:37 -0400

    use sab icon

--- notification.py
+++ notification.py
@@ -12,7 +12,7 @@ class NotificationClient(object):
                 cfg = json.load(w)
                 self._logger.debug("Notification Data loaded successfully" + str(cfg))
                 self._notifier = apprise.Apprise(asset=apprise.AppriseAsset(
-                    image_url_mask="https://avatars3.githubusercontent.com/u/3368377?s=200&v=4",
+                    image_url_mask="https://avatars.githubusercontent.com/u/960698?s=200&v=4",
                     default_extension=".jpeg"))
                 for k,v in cfg['notifications'].items():
                     if(v['enabled'] == True):
